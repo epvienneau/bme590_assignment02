7c4d8e1d660ef1498f26a63d995c13da1d4db9b8
epvienneau <epv4@duke.edu>
2017-09-21 19:58:03 -0400

    improved script to test for zero heart rate condition and use appropriate data types

--- test_bradyTachyCardia.py
+++ test_bradyTachyCardia.py
@@ -1,8 +1,10 @@
 from bradyTachyCardia import bradyTachyCardia as BTC
 
 def test_bradyTachyCardia():
-    testerArray = (100, 88, 101, 64, 160, 60, 59, 40, 71, 0, -1, 200, 90, 80, 62)
+    times = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    HRs = [0, 50, 90, 110, 70, 60, 100, 112, 40, 30, -1, 200, 64, 76]
+    testerArray = (times, HRs)
     BTClist = BTC(testerArray)
-    assert BTClist.size == testerArray.size()
-    assert BTClist.dim == testerArray.dim()
-    assert BTClist == ('T', 'N', 'T', 'N', 'T', 'B', 'B', 'B', 'N', 'B', 'B', 'T', 'N', 'N', 'N')
+    assert type(BTClist) is list
+    assert len(BTClist) == len(HRs)
+    assert BTClist == ('--', 'B', 'N', 'T', 'N', 'B', 'T', 'T', 'B', 'B', 'B', 'T', 'N', 'N')
