501c2e74b1b8fe9e6f4a25afef4bb4f58a2d3def
epvienneau <epv4@duke.edu>
2017-09-21 20:23:38 -0400

    Created script to write output to csv file

---
+++ write_output.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def write_output(time_HRinst, HRavg, btc):
+    """Writes to text file time, instantaneous heart rate, average heart rate over user-specified interval, and when
+    brady- or tachycardia occurred in the ECG trace
+    :param time_HRinst: ndarray where first element is times and second element is instantaneous heart rates
+    :param HRavg: list of average heart rates
+    :param btc: list of characters to indicate patient status (-- means not enough data, T means tachycardia,
+        B means bradycardia, and N means normal
+    :returns: nothing"""
+
+    file = open("assignment02_output", "w+")
+    header = "Time (s), Instantaneous Heart Rate, Average Heart Rate, Brady/Tachycardia Occurrence\n"
+    file.write(header)
+    for i in HRavg:
+        row = time_HRinst[1][i] + "," + time_HRinst[2][i] + "," + HRavg[i] + "," + btc[i] + "\n"
+        file.write(row)
