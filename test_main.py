6a25b16bfd7f8925f70278272d70ed209733b345
epvienneau <epv4@duke.edu>
2017-09-25 15:30:19 -0400

    Wrote test_main file to verify that output file was created when main wa run

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from main import main
+import os
+import time
+
+def test_main():
+    timeSinceEpoch = time.time()
+    main('Test_ECG.csv', 3)
+    timeStamp = os.path.getmtime('assignment02.csv')
+    assert timeStamp > timeSinceEpoch  #asserts that new file was actually created in calling of main function
+
