8594e9f5452129b7b4970518fc3f07eb9a34ef0d
epvienneau <epv4@duke.edu>
2017-09-25 00:41:37 -0400

    Updated main file with appropriate imports and function calls

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+from bradyTachyCardia import bradyTachyCardia
+from write_output import write_output
+from load_data import load_data
+from HRinst import HRinst
+from take_Average import average
+
+def main(filename):
+    """This file is the glue for our heart rate monitor software. It takes as input a file name for ECG data,
+    it calculates the instantaneous and average heart rates and when brady- or tachycardia occur, and it writes this 
+    data to another file.
+    :param filename: (str) name of the ECG file
+    :return: nothing
+    """
+    data = load_data(filename)
+    HRinst_output = HRinst(data)
+    HRavg_output = average(HRinst_output)
+    btc_output = bradyTachyCardia(HRinst_output)
+    write_output(HRinst_output, HRavg_output, btc_output)
+
+if __name__ == "__main__":
+    main('ECGTest.csv')
