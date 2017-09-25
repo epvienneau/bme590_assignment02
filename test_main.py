c8ca99440f4dd5de6d8c438a17dbba5492b8660c
epvienneau <epv4@duke.edu>
2017-09-25 15:52:35 -0400

    corrected file name

--- test_main.py
+++ test_main.py
@@ -5,5 +5,5 @@ import time
 def test_main():
     timeSinceEpoch = time.time()
     main('Test_ECG.csv', 3)
-    timeStamp = os.path.getmtime('assignment02.csv')
+    timeStamp = os.path.getmtime('assignment02_output.csv')
     assert timeStamp > timeSinceEpoch  #asserts that new file was actually created/modified in calling of main function
