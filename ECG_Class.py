4bfe82eec265f770fabe56f2374152199f4bc929
Julia Ross <jlr72@duke.edu>
2017-10-05 16:19:02 -0400

    fixed more stuff based on in class

--- ECG_Class.py
+++ ECG_Class.py
@@ -1,28 +1,33 @@
-from load_data import load_data
-from take_Average import average
-from HRinst import HRinst
-from bradyTachyCardia import bradyTachyCardia
-from write_output import write_output
-
 class ECG_Class(object):
 
     """This class treats a file containing ECG data as an object
 
     It has many associated methods that process and display this data"""
 
-    def __init__(self):
-        self.data = load_data(self)
+    def __init__(self, filename, avemins=1,outName="assignment02",lowerThresh=60,upperThresh=100):
+        from load_data import load_data
+        from HRinst import HRinst
+        self.name = filename[:-4]
+        self.mins = avemins
+        self.outputfile = outName
+        self.bradyT = lowerThresh
+        self.tachyT = upperThresh
+        self.data = load_data(self.name)
         self.time = self.data[:][0]
         self.voltage = self.data[:][1]
         self.instHR = HRinst(self.data)
 
     def avg(self):
-        return average(self.instHR,self.time,mins=1)
+        from take_Average import average
+
+        return average(self.instHR,self.time,self.mins)
 
     def bradyTachy(self):
-        return bradyTachyCardia(self.instHR)
+        from bradyTachyCardia import bradyTachyCardia
 
-    def output(self,mins=1,filename="assignment02_output.csv"):
+        return bradyTachyCardia(self.instHR,self.bradyT,self.tachyT)
+
+    def output(self,filename="assignment02_output.csv"):
         """ Creates a file containing the output of these functions
 
         Runs all of the functions and outputs into a file of the specified filename
@@ -35,6 +40,11 @@ class ECG_Class(object):
             An ndarray of average heart rate at each time point
         """
 
-        ave = average(self.instHR,self.time,mins)
-        btc = bradyTachyCardia(self)
+        from write_output import write_output
+        if self.outputfile == "assignment02":
+            oName = self.name + '_output.txt'
+
+        ave = self.avg()
+        btc = self.btc()
         return write_output(self.time, self.HRinst, ave, btc, filename)
+
