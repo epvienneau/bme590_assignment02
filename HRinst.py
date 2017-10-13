15ad7d4488587ac9f9b2ff9465b4bf6420296492
Pablo Ortiz <poloritz4@hotmail.com>
2017-10-13 00:15:14 -0400

    Address Dr. Palmeris comments on HRinst

--- HRinst.py
+++ HRinst.py
@@ -2,21 +2,22 @@ import numpy as np
 from scipy import stats
 
 
-def HRinst(dataset,secperunit=60):
+def HRinst(dataset, secperunit=60, peak_threshold=0.5):
 
     """
     Takes the input data of the time and voltage to convert it into an array with time and instantaneous heart rate.
 
     :param dataset: (tuple) Two elements, each a 1xN ndarray for time and voltage values respectively
     :param secperunit: (int or double) Conversion from unit of time ndarray to seconds
+    :param peak_threshold: (double) percentage of maximum peak to set thresholding
     :returns: (ndarray) 2 columns. First column with time in s, second column with heart rate in BPM.
         Each element in the ndarray is a float.
     """
 
     time = dataset[:][0]
     voltage = dataset[:][1]
-    thresholded = stats.threshold(voltage, 0.5 * voltage.max())
-    HRinst = np.zeros(len(thresholded))
+    thresholded = stats.threshold(voltage, peak_threshold * voltage.max())
+    hrinst = np.zeros(len(thresholded))
 
     is_increasing = np.roll(thresholded, 1) <= thresholded
     will_decrease = np.roll(thresholded, -1) < thresholded
@@ -26,10 +27,10 @@ def HRinst(dataset,secperunit=60):
     for i,val in enumerate(thresholded):
         peaks=peakInd[peakInd<i]
         if i>peakInd[0][1]:
-            HRinst[i] = secperunit/ (time[int(peaks[-1])] - time[int(peaks[-2])])
+            hrinst[i] = secperunit/ (time[int(peaks[-1])] - time[int(peaks[-2])])
         else:
-            HRinst[i]=0
+            hrinst[i]=0
 
-    HRinst[-1]=HRinst[-2]
-    HRinst=np.column_stack((time,HRinst))
-    return HRinst
+    hrinst[-1]=hrinst[-2]
+    hrinst=np.column_stack((time,hrinst))
+    return hrinst
